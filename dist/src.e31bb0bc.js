0adfe272124bc14928d9d057bf2d94607366911f
erichvorster <erichvorster1@gmail.com>
2022-02-07 23:17:05 +0200

    4th commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8286,7 +8286,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58181" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60939" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
